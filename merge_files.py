20aada55c4200944d2b751fcbd294e2794ba399e
Brian McEntee <brian.m.mcentee@gmail.com>
2016-04-15 22:48:09 -0400

    fix rounding issues by avoiding converting data to a float

--- merge_files.py
+++ merge_files.py
@@ -6,7 +6,7 @@ def get_data(file_name) :
     data = []
     for line in lines:
         split_line = line.split(',')
-        data.append([int(split_line[0]),float(split_line[1])])
+        data.append([int(split_line[0]),split_line[1].strip('\r\n')])
     return data
 
 def merge_to_csv(d, file_name) :
@@ -14,7 +14,7 @@ def merge_to_csv(d, file_name) :
     f.write('ID,x,y\r\n')
     key_list = d.keys()
     key_list.sort()
-    format_string = "%d,%f,%f\r\n"
+    format_string = "%d,%s,%s\r\n"
     for key in key_list:
         f.write(format_string % (key,d[key][0], d[key][1]))
 
@@ -23,7 +23,7 @@ def removed_to_csv(d, var,file_name) :
     f.write('ID,%s\r\n' % var)
     key_list = d.keys()
     key_list.sort()
-    format_string = "%d,%f\r\n"
+    format_string = "%d,%s\r\n"
     for key in key_list:
         f.write(format_string % (key,d[key]))
 
@@ -31,6 +31,7 @@ def main() :
     # Get lists of data from each file
     x_data = get_data("x.csv")
     y_data = get_data("y.csv")
+    print x_data 
     # Get IDs for each set of values
     x_ids = [x_data[i][0] for i in range(len(x_data))]
     y_ids = [y_data[i][0] for i in range(len(y_data))]
